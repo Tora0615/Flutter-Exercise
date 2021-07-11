b17951be983a0f90b541980688bc1db796c6a732
Tora0615 <5438arthur@gmail.com>
2021-07-11 18:33:53 +0800

    [Info] Down simple PopupMenuButton exercise.

--- widgets/popupmenubutton/README.md
+++ widgets/popupmenubutton/README.md
@@ -1,16 +1,11 @@
 # popupmenubutton
+基礎的樣子長這樣 : 
 
-A new Flutter application.
+![](.README_images/01.png) --> 
+![](.README_images/02.png)
 
-## Getting Started
+### 之後有空嘗試看看這個
+![](.README_images/03.png)
 
-This project is a starting point for a Flutter application.
 
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+[Flutter Tutorial - PopupMenuButton - Focused Menu](https://www.youtube.com/watch?v=Vttl7WGtkgg&ab_channel=JohannesMilke)
--- widgets/popupmenubutton/lib/main.dart
+++ widgets/popupmenubutton/lib/main.dart
@@ -4,3 +4,48 @@ void main() {
   runApp(MyApp());
 }
 
+//enum enum_name { enumeration list }
+//enumeration list 由逗點隔開，第一個的 index 為 0
+enum Items {One, Two, Three, Four}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("PopupMenuButton練習"),),
+        body: Center(
+          child: TextButton(
+            onPressed: (){
+            },
+            child: PopupMenuButton<Items>(
+              onSelected: (Items result){
+                print(result);
+              },
+              itemBuilder: (BuildContext context){
+                return [
+                  PopupMenuItem(
+                    child: Text("選項一"),
+                    value: Items.One,
+                  ),
+                  PopupMenuItem(
+                    child: Text("選項二"),
+                    value: Items.Two,
+                  ),
+                  PopupMenuItem(
+                    child: Text("選項三"),
+                    value: Items.Three,
+                  ),
+                  PopupMenuItem(
+                    child: Text("選項四"),
+                    value: Items.Four,
+                  ),
+                ];
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
