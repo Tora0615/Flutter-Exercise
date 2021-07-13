44fc7cdf00c6073551ac72c5a4a81ec631bf168b
Tora0615 <5438arthur@gmail.com>
2021-07-13 21:08:05 +0800

    [Info] Add BoxFit.none and BoxFit.fill.

--- widgets/fittedbox/lib/main.dart
+++ widgets/fittedbox/lib/main.dart
@@ -16,9 +16,55 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
 
+                // BoxFit.none
+                Column(
+                  children: [
+                    Container(
+                      color: Colors.amber,
+                      width: 100,
+                      height: 100,
+                      child: FittedBox(
+                        fit: BoxFit.none,
+                        alignment: Alignment.topLeft,
+                        child: Text("文字文字文字")
+                      ),
+                    ),
+                    Text("BoxFit.none"),
+                    Text("說明 : 無填充")
+                  ],
+                ),
+
+                //BoxFit.fill
+                Column(
+                  children: [
+                    Container(
+                      color: Colors.amber,
+                      width: 100,
+                      height: 100,
+                      child: FittedBox(
+                          fit: BoxFit.fill,
+                          alignment: Alignment.topLeft,
+                          child: Text("文字文字文字")
+                      ),
+                    ),
+                    Text("BoxFit.fill"),
+                    Text("說明 : 不管寬高比，盡\n量填滿，不超出範圍")
+                  ],
+                ),
+
+
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
