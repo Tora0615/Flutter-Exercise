7023337c5a527a5b6157ef70c471c54b70d4772c
Tora0615 <5438arthur@gmail.com>
2021-07-14 21:00:44 +0800

    [Info] Initialization BaseLine and OffStage exercise.

---
+++ widgets/baseline/android/app/src/main/kotlin/com/example/baseline/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.baseline
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ widgets/offstage/android/app/src/main/kotlin/com/example/offstage/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.offstage
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
