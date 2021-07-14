312f24979db2e80431f321158af6e1d8e4410c70
Tora0615 <5438arthur@gmail.com>
2021-07-14 21:59:51 +0800

    [Info] OffStage exercise done.

--- widgets/offstage/lib/main.dart
+++ widgets/offstage/lib/main.dart
@@ -4,3 +4,40 @@ void main() {
   runApp(MyApp());
 }
 
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  bool offstage = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SafeArea(
+        child: Scaffold(
+
+          body: Center(
+            child: Offstage(
+              offstage: offstage,
+              child: Text("文字文字文字",style: TextStyle(fontSize: 30),),
+            ),
+          ),
+
+          floatingActionButton: FloatingActionButton(
+            onPressed: (){
+              print("pressed");
+              setState(() {
+                offstage = !offstage;
+              });
+            },
+            tooltip: "顯示/隱藏",
+            child: Icon(Icons.flip),
+          ),
+        ),
+      ),
+    );
+  }
+}
