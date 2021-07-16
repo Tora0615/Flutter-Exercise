664932015519f5eb9b582a4e8f189505501e9170
Tora0615 <5438arthur@gmail.com>
2021-07-16 21:50:25 +0800

    [Info] Done Linear Gradient exercise.

--- widgets/lineargradient/lib/main.dart
+++ widgets/lineargradient/lib/main.dart
@@ -4,3 +4,96 @@ void main() {
   runApp(MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Column(
+                children: [
+                  Text("begin : (0.0,0.0)\nend : (1.0,1.0)"),
+                  Center(
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          begin: FractionalOffset(0.0,0.0),
+                          end: FractionalOffset(1.0,1.0),
+                          colors: [
+                            Colors.red,
+                            Colors.amber,
+                            Colors.green,
+                            Colors.blueAccent
+                          ],
+                        ),
+                      ),
+                      child: Container(
+                        width: 100,
+                        height: 100,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+
+              Column(
+                children: [
+                  Text("begin : (0.5,0.5)\nend : (1.0,1.0)"),
+                  Center(
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          begin: FractionalOffset(0.5,0.5),
+                          end: FractionalOffset(1.0,1.0),
+                          colors: [
+                            Colors.red,
+                            Colors.amber,
+                            Colors.green,
+                            Colors.blueAccent
+                          ],
+                        ),
+                      ),
+                      child: Container(
+                        width: 100,
+                        height: 100,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+
+              Column(
+                children: [
+                  Text("begin : (1.0,0.0)\nend : (1.0,1.0)"),
+                  Center(
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          begin: FractionalOffset(1.0,0.0),
+                          end: FractionalOffset(1.0,1.0),
+                          colors: [
+                            Colors.red,
+                            Colors.amber,
+                            Colors.green,
+                            Colors.blueAccent
+                          ],
+                        ),
+                      ),
+                      child: Container(
+                        width: 100,
+                        height: 100,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
