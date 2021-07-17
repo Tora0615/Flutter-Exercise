5235d8a3b646c471c8ee38a1ecf54c3d4525cede
Tora0615 <5438arthur@gmail.com>
2021-07-17 22:50:54 +0800

    [Info] Add database.dart.

---
+++ features/sqlite/database.dart
@@ -0,0 +1,11 @@
+import 'dart:async';
+import 'dart:io';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+//import 'client.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DBProvider{
+  // 私有構造函數
+  DBProvider._();
+}
