f2c558eeed36406e6155df32398b7da1f8fed1fe
Tora0615 <5438arthur@gmail.com>
2021-07-18 21:26:38 +0800

    [Info] Refactor database.dart and creat client.dart for client class.

---
+++ features/sqlite/lib/client.dart
@@ -0,0 +1,10 @@
+class Client{
+  int? id;
+  String? name;
+  int? age;
+  bool? sex;
+
+  Client({this.name,this.age,this.id,this.sex});
+
+  
+}
--- features/sqlite/lib/database.dart
+++ features/sqlite/lib/database.dart
@@ -41,5 +41,7 @@ class DBProvider {
     });
   }
 
-  
+  insertClient (Client newClient) async {
+    final db = await database;
+  }
 }
