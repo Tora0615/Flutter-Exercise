c9d289422e5fd3a8e17e6b06b6c4ed7bc8c25c35
Tora0615 <5438arthur@gmail.com>
2021-07-21 17:04:58 +0800

    [Info] Done database.dart

--- features/sqlite/lib/database.dart
+++ features/sqlite/lib/database.dart
@@ -54,16 +54,68 @@ class DBProvider {
   //新增Client
   insertClient(Client newClient) async {
     final db = await database;
+
+    // rawQuery 是直接用 SQL 語句查詢
     var table = await db.rawQuery("SELECT MAX(id)+1 as id FROM Client");
-    int id = table.first["id"];
+    var id = table.first["id"];
 
     var raw = await db.rawInsert(
-      "INSERT Into Client (id,name,age,sex)"
-      "VALUES (?,?,?,?)",
-      [id, newClient.name, newClient.age, newClient.sex]
-    );
+        "INSERT Into Client (id,name,age,sex)"
+        "VALUES (?,?,?,?)",
+        [id, newClient.name, newClient.age, newClient.sex]);
 
-    print ("raw" + raw.toString());
+    print("raw" + raw.toString());
     return raw;
   }
+
+  updateSex(Client client) async {
+    final db = await database;
+    Client tempClient = Client(
+      id: client.id,
+      name: client.name,
+      age: client.age,
+      sex: !client.sex!, //反轉性別
+    );
+    var res = await db.update("Client", tempClient.toMap(),
+        where: "id = ?", whereArgs: [client.id]);
+    return res;
+  }
+
+  updateClient (Client newClient) async {
+    final db = await database;
+    var res = await db.update("Client", newClient.toMap(),
+        where: "id = ?", whereArgs: [newClient.id]);
+    return res;
+  }
+  
+  getClient(int id) async {
+    final db = await database;
+    var res = await db.query("Client", where: "id = ?",whereArgs: [id] );
+    return res.isNotEmpty ? Client.fromMap(res.first) : null ;
+  }
+
+  Future<List<Client>> getAllClients() async {
+    final db = await database;
+    var res = await db.query("Client");
+
+    // 將 res 這可迭代的東東，利用.map，經內部function逐一轉換處理，
+    // 再把迭代處理完這坨利用.toList()轉型成List
+    List<Client> list = res.isNotEmpty ? res.map((e) => Client.fromMap(e)).toList() : [] ;
+    return list;
+  }
+
+  deleteClient(int id) async{
+    final db = await database;
+    return db.delete("Client", where: "id = ?", whereArgs: [id]);
+  }
+
+  deleteAll() async{
+    final db = await database;
+    db.rawDelete("Delete * from Client");
+  }
+
 }
+
+// 參考
+// SQLiteDatabase query 和 rawQuery 的区别
+// https://blog.csdn.net/chaoyu168/article/details/50350392
