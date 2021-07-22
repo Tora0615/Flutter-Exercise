d310e6dfe19c073c5518a3d8e28447f162b5a87d
Tora0615 <5438arthur@gmail.com>
2021-07-22 09:34:07 +0800

    [Info] Add comments of all files.

--- features/sqlite/lib/main.dart
+++ features/sqlite/lib/main.dart
@@ -13,6 +13,8 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+
+  // floatingActionButton 的模板
   List<Client> clients = [
     Client(name: "張三", age: 20, sex: false),
     Client(name: "李四", age: 22, sex: true),
@@ -25,14 +27,25 @@ class _MyAppState extends State<MyApp> {
       appBar: AppBar(
         title: Text("sqflite 範例"),
       ),
+
+      // FutureBuilder : 確認未來會有資料回來，再渲染的 widget builder
       body: FutureBuilder<List<Client>>(
+
+        // 取得所有Client
         future: DBProvider.db.getAllClients(),
         builder: (BuildContext context, AsyncSnapshot<List<Client>> snapshot){
+
+          // 有拿到資料
           if (snapshot.hasData) {
             return ListView.builder(
+
+              // 資料個數
               itemCount: snapshot.data!.length,
+              // 依資料個數 return 對應數量 widget 的 builder
               itemBuilder: (BuildContext context, int index) {
+                // 當下某個的 Client 資料
                 Client item = snapshot.data![index];
+                // Dismissible 是可以滑動刪除的widget
                 return Dismissible(
                   key: UniqueKey(),
                   background: Container(
@@ -41,10 +54,12 @@ class _MyAppState extends State<MyApp> {
                   onDismissed: (direction) {
                     DBProvider.db.deleteClient(item.id!);
                   },
+                  // 每比實際資料的顯現
                   child: ListTile(
                     title: Text(item.name.toString()),
                     leading: Text(item.id.toString()),
                     trailing: Checkbox(
+                      // 切換性別
                       onChanged: (bool? value) {
                         DBProvider.db.updateSex(item);
                         setState(() {});
@@ -56,6 +71,8 @@ class _MyAppState extends State<MyApp> {
               },
             );
           }
+
+          // 沒拿到資料，預設顯示轉圈圈的緩衝動畫
           else{
             return Center(
               child: CircularProgressIndicator(),
@@ -67,7 +84,9 @@ class _MyAppState extends State<MyApp> {
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () async {
+          // 從模板 clients 中隨機抽取一個 Client Object
           Client rnd = clients [math.Random().nextInt(clients.length)];
+          // 插入 database 中
           await DBProvider.db.insertClient(rnd);
           setState(() {
 
