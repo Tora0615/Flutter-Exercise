c634c89bb79e1c128b36022b697135346f7d63a8
Tora0615 <5438arthur@gmail.com>
2021-07-22 09:32:05 +0800

    [Info] Change Clinent.dart's toMap() function.

--- features/sqlite/lib/client.dart
+++ features/sqlite/lib/client.dart
@@ -8,23 +8,23 @@ class Client{
 
   Client({this.name,this.age,this.id,this.sex});
 
-  // 將 Json 轉成數據模型
-  factory Client.fromMap( Map <String, dynamic> json ){ //似乎是名為json的 string : 動態變數 的形式(map)
+  // 將 Map 形式轉成 Client 數據型式並回傳
+  factory Client.fromMap( Map <String, dynamic> json ){ //名為 json 的 map 形式(string : 動態變數)
     return Client(
       id : json["id"],
       name : json["name"],
       age: json["age"],
-      sex: json["sex"] == 1,
+      sex: json["sex"] == 1,  // sqlite 不能存 bool，因此用回來的 bit 變數比對，看是 true 還是 false。 (設 1 為男)
     );
   }
 
-  // 將數據模型轉成 Json
+  // 將數據模型轉成 Map 形式 (DB處理會用到)
   Map <String, dynamic> toMap() {
     return {  //回傳一坨 Map 數據
       "id" : id,
       "name" : name,
       "age" : age,
-      "sex" : sex,
+      "sex" : sex! ? 1 : 0,  // sqlite 不能存 bool，因此用 true 還是 false，轉換為 bit 的 0 or 1。 (設 1 為男)
     };
   }
 
