fd1d71cedb8993aecc500110833d1b8ec5e6406b
Tora0615 <5438arthur@gmail.com>
2021-07-23 11:55:08 +0800

    [Info] Add different size and container's experience.

--- features/hero_page_switch/lib/main.dart
+++ features/hero_page_switch/lib/main.dart
@@ -19,21 +19,49 @@ class MainScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Main Screen'),
       ),
-      body: GestureDetector(
-        onTap: () {
-          Navigator.push(context, MaterialPageRoute(builder: (_) {
-            return DetailScreen();
-          }));
-        },
+      body: Column(
+        children: [
 
-        // Hero 要包裹相同組件，且tag相同，切換時會自然地過渡過去
-        child: Hero(
-          tag: 'imageHero',
-          child: Image.network(
-            'https://picsum.photos/250?image=9',
-            scale: 2,
+          // 圖片練習
+          GestureDetector(
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (_) {
+                return DetailScreen();
+              }));
+            },
+            child: Hero(
+              tag: 'imageHero',
+              child: Image.network(
+                'https://picsum.photos/250?image=9',
+                scale: 2,
+              ),
+            ),
           ),
-        ),
+
+          SizedBox(
+            height: 100,
+          ),
+
+          // container
+          GestureDetector(
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (_) {
+                return ContainerScreen();
+              }));
+            },
+            child: Hero(
+              tag: 'Container',
+              child: Container(
+                color: Colors.amber,
+                child: SizedBox(
+                  height: 125,
+                  width: 125,
+                ),
+              )
+            ),
+          ),
+
+        ],
       ),
     );
   }
@@ -48,7 +76,6 @@ class DetailScreen extends StatelessWidget {
           Navigator.pop(context);
         },
         child: Center(
-
           // Hero 要包裹相同組件，且tag相同，切換時會自然地過渡過去
           child: Hero(
             tag: 'imageHero',
@@ -62,3 +89,28 @@ class DetailScreen extends StatelessWidget {
   }
 }
 
+class ContainerScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GestureDetector(
+        onTap: () {
+          Navigator.pop(context);
+        },
+        child: Center(
+          // Hero 要包裹相同組件，且tag相同，切換時會自然地過渡過去
+          child: Hero(
+            tag: 'Container',
+            child: Container(
+              color:Colors.red,
+              child: SizedBox(
+                height: 300,
+                width: 300,
+              ),
+            )
+          ),
+        ),
+      ),
+    );
+  }
+}
