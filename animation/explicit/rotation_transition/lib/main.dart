6c7e74412a2b0b3341f242d0aea6563e7879045e
Tora0615 <5438arthur@gmail.com>
2021-07-24 13:49:13 +0800

    [Add] Rotation_transition/main.dart 1. Done all animation functions.

--- animation/explicit/rotation_transition/lib/main.dart
+++ animation/explicit/rotation_transition/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,9 +12,56 @@ class MyApp extends StatefulWidget {
   _MyAppState createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp> {
+
+
+class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
+  AnimationController? _animationController;
+
+  @override
+  void initState() {
+    _animationController = AnimationController(
+      duration: Duration(seconds: 5),
+      vsync: this,
+    );
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _animationController!.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: SafeArea(
+        child: Scaffold(
+          body: Center(
+            child: RotationTransition(
+              child: Container(
+                width: 200,
+                height: 200,
+                color: Colors.amber,
+              ),
+              alignment: Alignment.center,
+              turns: _animationController!,
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            child: Icon(
+              Icons.rotate_right,
+            ),
+            onPressed: (){
+              if (_animationController!.isAnimating) {
+                _animationController!.stop();
+              } else {
+                _animationController!.repeat();
+              }
+            },
+          ),
+        ),
+      ),
+    );
   }
 }
