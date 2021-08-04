1daef16de858e9dc1ad9fed2616fe17d3cb954ff
Tora0615 <5438arthur@gmail.com>
2021-08-05 00:02:27 +0800

    feat : change some comment.

--- features/firebase_brew_crew_project/lib/services/auth.dart
+++ features/firebase_brew_crew_project/lib/services/auth.dart
@@ -7,14 +7,15 @@ class AuthService {
   // 可以利用 _auth 操作 FirebaseAuth 這個 class 裡不同屬性跟功能
 
   // sign in anon (anonymous)
-  // 是個非同步事件，並且會回傳 Future
-  Future signInAnon() async {
-    // 嘗試登入
-    try{
+  Future signInAnon() async {  // 是個非同步事件，並且會回傳 Future
+    try{ //嘗試登入
+      // 呼叫匿名登入的 function
       UserCredential result = await _auth.signInAnonymously();  // 與影片不同
-      User? user = result.user;  // 與影片不同
+
+      //從 result 中取出 user
+      User? user = result.user;    // 與影片不同
       return user;
-    }catch(e){
+    }catch(e){ //登入失敗
       print(e.toString());
       return null;
     }
