86d9394706355012a9b476583372b1bf610064da
Tora0615 <5438arthur@gmail.com>
2021-08-06 00:11:02 +0800

    feat : Add a stream to listen authStateChanges in the auth.dart file.

--- features/firebase_brew_crew_project/lib/main.dart
+++ features/firebase_brew_crew_project/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_brew_crew_project/screens/wrapper.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'services/auth.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
--- features/firebase_brew_crew_project/lib/services/auth.dart
+++ features/firebase_brew_crew_project/lib/services/auth.dart
@@ -2,30 +2,38 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_brew_crew_project/models/user.dart';
 
 class AuthService {
-
   // firebase auth object
   final FirebaseAuth _auth = FirebaseAuth.instance; //private object
   // 可以利用 _auth 操作 FirebaseAuth 這個 class 裡不同屬性跟功能
 
-
   // a function create user obj based on firebase user class
-  CustomUser? _userFormFirebaseUser(User? user){  //
-    return user != null ? CustomUser(uid: user.uid) : null ;
+  CustomUser? _userFormFirebaseUser(User? user) {
+    //
+    return user != null ? CustomUser(uid: user.uid) : null;
   }
   // 輸入值 User? user : 因為可能傳 null 進來，因此要加 "?" 。
   // 回傳值 CustomUser? : 因為可能回傳 null ，因此要加 "?" 。
 
+  // auth change user stream
+  Stream<CustomUser?> get user {
+    return _auth
+        .authStateChanges()
+        .map(_userFormFirebaseUser); // 等同於 .map((User? user) => _userFormFirebaseUser(user));
+  }
 
   // sign in anon (anonymous)
-  Future signInAnon() async {  // 是個非同步事件，並且會回傳 Future
-    try{ //嘗試登入
+  Future signInAnon() async {
+    // 是個非同步事件，並且會回傳 Future
+    try {
+      //嘗試登入
       // 呼叫匿名登入的 function
       UserCredential result = await _auth.signInAnonymously();
 
       //從 result 中取出 user
       User? user = result.user;
-      return _userFormFirebaseUser(user);  // 原本是 return user;
-    }catch(e){ //登入失敗
+      return _userFormFirebaseUser(user); // 原本是 return user;
+    } catch (e) {
+      //登入失敗
       print(e.toString());
       return null;
     }
@@ -37,4 +45,3 @@ class AuthService {
 
   // sign out
 }
-
