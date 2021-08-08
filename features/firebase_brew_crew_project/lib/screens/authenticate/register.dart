a90ae129036af5fae540868a9b99b935f445f6de
Tora0615 <5438arthur@gmail.com>
2021-08-08 14:34:30 +0800

    feat : Done the Form and TextFormField validator.

--- features/firebase_brew_crew_project/lib/screens/authenticate/register.dart
+++ features/firebase_brew_crew_project/lib/screens/authenticate/register.dart
@@ -14,6 +14,11 @@ class _RegisterState extends State<Register> {
 
   final AuthService _auth = AuthService();
 
+  // 用來持續追蹤此 form 與他的狀態。
+  // 未來如果嘗試驗證 form 的內容，就可以經由此 form key 達成。
+  // 因為利用此 key 就能訪問資料與狀態。
+  final _formKey = GlobalKey<FormState>();
+
   // Text field temp saving
   String email = "";
   String password = "";
@@ -51,6 +56,7 @@ class _RegisterState extends State<Register> {
         // 可以統一操作只能是 FormField 類型。
         // 參考 : https://book.flutterchina.club/chapter3/input_and_form.html#_3-7-1-textfield
         child: Form(
+          key: _formKey,  // 將 _formKey 與此 Form 做關聯。為其他 TextFormField 的 global key
           child: Column(
             children: [
               SizedBox(
@@ -60,6 +66,16 @@ class _RegisterState extends State<Register> {
                 onChanged: (val) {  // 每次文字改變(新增、刪除等)
                   email = val ;  // 影片有用 setState，但我覺得不用，目前可 print 出來
                 },
+
+                // validator 會回傳字串(輸入有問題)或null(輸入沒問題)，
+                // 此字串是會在螢幕上顯示的幫助文字。
+                validator: (val){
+                  if(val!.isEmpty){
+                    return "請輸入 Email" ;
+                  }else{
+                    return null;
+                  }
+                },
               ),
               SizedBox(
                 height: 20,
@@ -68,6 +84,15 @@ class _RegisterState extends State<Register> {
                 obscureText: true, // 輸入文字隱藏
                 onChanged: (val) {  // 每次文字改變(新增、刪除等)
                   password = val ; // 影片有用 setState，但我覺得不用，目前可 print 出來
+                },
+
+                // validator 會回傳字串(輸入有問題)或null(輸入沒問題)，
+                validator: (val){
+                  if(val!.length < 6){
+                    return "請輸入至少六位數的密碼" ;
+                  }else{
+                    return null;
+                  }
                 },
               ),
               SizedBox(
@@ -82,8 +107,15 @@ class _RegisterState extends State<Register> {
                   style: TextStyle(color: Colors.white),
                 ),
                 onPressed: () async {
-                  print(email);
-                  print(password);
+                  // 會在按下按鈕時，呼叫 _formKey.currentState!.validate() 時
+                  // 其會去呼叫 TextFormField 中 validator 內的函式，
+                  // 去檢查輸入內容是否符合標準。若都正常(回傳null)，
+                  // _formKey.currentState!.validate() 最終會返回 true。
+                  // true 代表格式 OK，之後就會執行裡面的內容。
+                  if (_formKey.currentState!.validate()){
+                    print(email); // 測試用
+                    print(password);
+                  }
                 },
               ),
             ],
